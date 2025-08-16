fe7aacc06cd7efc02e37604c1557b7d79ddc7304
Caio Marinho <108732958+Caio-Marinho@users.noreply.github.com>
2025-08-16 18:29:44 -0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Seleciona o formulário
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita envio padrão
+
+  // Pega os valores dos inputs e selects
+  const nome = document.getElementById('nome').value.toLowerCase();
+  const sobrenome = document.getElementById('sobrenome').value.toLowerCase();
+  const comite = document.getElementById('comite').value;
+  const cargo = document.getElementById('cargo').value;
+  const area = document.getElementById('area').value;
+  const codigo = document.getElementById('codigo').value;
+
+  // Exibe os valores no console
+  console.log('Nome:', nome);
+  console.log('Sobrenome:', sobrenome);
+  console.log('Comitê:', comite);
+  console.log('Cargo:', cargo);
+  console.log('Área:', area);
+  console.log('Código de Membro:', codigo);
+  alert(`O e-mail ${nome.split(" ")[0]}.${sobrenome.split(" ").pop()}@aiesec.org.br, pertencente a ${nome} ${sobrenome} da aiesec ${comite} foi gerado com sucesso.`);
+  
+  // Aqui você pode enviar os dados para um servidor via fetch/ajax
+  
+  form.reset();
+});
