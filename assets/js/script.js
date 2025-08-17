3c6e4322d0cf8637233641ea3d69ee3c14ac44eb
Caio-Marinho <kaigabriel12@gmail.com>
2025-08-17 16:09:05 -0300

    aplicação de validação de codigo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,51 @@
 // Seleciona o formulário
 const form = document.querySelector('#email');
+// Pega os valores dos inputs e selects
+const nome = document.querySelector('#nome');
+const sobrenome = document.querySelector('#sobrenome');
+const comite = document.querySelector('#comite');
+const cargo = document.querySelector('#cargo');
+const area = document.querySelector('#area');
+const codigo = document.querySelector('#codigo');
+const erroDigitacao = document.querySelector("#erro_codigo");
+erroDigitacao.textContent = "Use números . Digite seu codigo de membresia.";
+
+codigo.addEventListener('input', () => {
+    // Mantém apenas números
+    codigo.value = codigo.value.replace(/[^0-9]/g, '');
+    
+    // Valida o campo
+    if (codigo.value !== '' && !isNaN(codigo.value) && codigo.value.length === 5) {
+      codigo.classList.add('codigo-valid');
+      codigo.classList.remove('codigo-invalid');
+      erroDigitacao.textContent = " ";
+    } else if (isNaN(codigo.value) && codigo.value.length === 5 || codigo.value.length > 0){
+      codigo.classList.add('codigo-invalid');
+      codigo.classList.remove('codigo-valid');
+      erroDigitacao.textContent = "O codigo de Membresia tem 5 digitos";
+    } else {
+      codigo.classList.remove('codigo-invalid');
+      codigo.classList.remove('codigo-valid');
+      erroDigitacao.textContent = "Use números . Digite seu codigo de membresia.";
+    }
+  });
 
 form.addEventListener('submit', function(event) {
   event.preventDefault(); // Evita envio padrão
-
-  // Pega os valores dos inputs e selects
-  const nome = document.getElementById('nome').value.toLowerCase().trim();
-  const sobrenome = document.getElementById('sobrenome').value.toLowerCase().trim();
-  const comite = document.getElementById('comite').value;
-  const cargo = document.getElementById('cargo').value;
-  const area = document.getElementById('area').value;
-  const codigo = document.getElementById('codigo').value;
-
+  const inputNome = nome.value.toLowerCase().trim();
+  const inputSobrenome = sobrenome.value.toLowerCase().trim();
+  const inputComite = comite.value;
+  const inputCargo = cargo.value;
+  const inputArea = area.value;
+  const inputCodigo = codigo.value
   // Exibe os valores no console
-  console.log('Nome:', nome);
-  console.log('Sobrenome:', sobrenome);
-  console.log('Comitê:', comite);
-  console.log('Cargo:', cargo);
-  console.log('Área:', area);
-  console.log('Código de Membro:', codigo);
-  alert(`O e-mail ${nome.split(" ")[0]}.${sobrenome.split(" ").pop()}@aiesec.org.br, pertencente a ${nome} ${sobrenome} da aiesec ${comite} foi gerado com sucesso.`);
+  console.log('Nome:', inputNome);
+  console.log('Sobrenome:', inputSobrenome);
+  console.log('Comitê:', inputComite);
+  console.log('Cargo:', inputCargo);
+  console.log('Área:', inputArea);
+  console.log('Código de Membro:', inputCodigo);
+  alert(`O e-mail ${inputNome.split(" ")[0]}.${inputSobrenome.split(" ").pop()}@aiesec.org.br, pertencente a ${inputNome} ${inputSobrenome} da aiesec ${inputComite} foi gerado com sucesso.`);
   
   // Aqui você pode enviar os dados para um servidor via fetch/ajax
   
